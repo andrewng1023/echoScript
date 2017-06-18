11bf9d765d785a8269a2d2be7ccfb2a75e5b240e
Andrew Ng <andrewng@Andrews-MacBook-Pro-3.local>
2017-06-18 13:28:50 -0700

    Added better filename writing and enzyme recognition

--- echoScript.py
+++ echoScript.py
@@ -6,17 +6,17 @@
 
 # # Master Mix Recipe
 #
-# Perform GG reactions in a 2 µl total volume:
+# Perform GG reactions in a 2 ul total volume:
 #
-# 0.1 µl BsaI/BsmBI (Esp3I)
+# 0.1 ul BsaI/BsmBI (Esp3I)
 #
 # 0.1 T4 DNA Ligase
 #
 # 0.2 10x Ligase Buffer + PEG
 #
-# 1.2 µl H20 (assume ~4 parts per rxn, can modify this if there is larger variance between parts)
+# 1.2 ul H20 (assume ~4 parts per rxn, can modify this if there is larger variance between parts)
 
-# Do rxns in a 2 µl volume (Prepare a master mix based on the number of reactions,
+# Do rxns in a 2 ul volume (Prepare a master mix based on the number of reactions,
 # also accounting for the minimum dead volume of the plate
 
 import numpy as np
@@ -25,6 +25,7 @@ from string import ascii_uppercase
 import cPickle as pkl
 import time
 import argparse
+import os.path
 
 #Import the csv
 def import_instructions(fname):
@@ -128,10 +129,15 @@ def checksourceplate(counts, sname, platetype):
 
 def makepicklists(rawdf, allsourcepl,dname):
     #Define 96 well plate coordinates
-    coord = list()
+    coord_ninetysix = list()
     for char in ascii_uppercase[0:8]:
         for col in range(12):
-            coord.append(char+str(col+1))
+            coord_ninetysix.append(char+str(col+1))
+
+    coord_threeeightyfour = list()
+    for char in ascii_uppercase[0:16]:
+        for col in range(24):
+            coord_threeeightyfour.append(char+str(col+1))
 
     #Define an empty picklist
     plasmidpicklist = pd.DataFrame(columns = ['Source Plate Name', 'Source Well', 'Destination Plate Name', 'Destination Well', 'Transfer Volume'])
@@ -149,18 +155,41 @@ def makepicklists(rawdf, allsourcepl,dname):
                     if allsourcepl[plate][well] is None:
                         continue
                     if allsourcepl[plate][well].keys()[0] == part:
-                        plasmidpicklist.loc[j] = [plate,well,'Destination[1]',coord[i],100] #hardcode in to do less than 96 GGs at once for now
+                        plasmidpicklist.loc[j] = [plate,well,'Destination[1]',coord_ninetysix[i],100] #hardcode in to do less than 96 GGs at once for now
                         allsourcepl[plate][well][part] -= .1
                         j+=1
-        destinationloc[rawdf.loc[i].GoldenGate.rstrip()] = coord[i]
+        destinationloc[rawdf.loc[i].GoldenGate.rstrip()] = coord_ninetysix[i]
+
         #Assume a regular dead volume plate
-        mmpicklist.loc[i] = ['Source[1]',coord[i/29],'Destination[1]',coord[i],1800]
+        if rawdf.loc[i].Enzyme == 'BsaI':
+            mmpicklist.loc[i] = ['Source[1]',coord_threeeightyfour[i/29],'Destination[1]',coord_ninetysix[i],1800]
+        else:
+            mmpicklist.loc[i] = ['Source[1]',coord_threeeightyfour[i/29 + 24],'Destination[1]',coord_ninetysix[i],1800]
 
     date = time.strftime("%Y%m%d")
-    plasmidpicklist.to_csv(date+'plasmidpicklist.csv',index=False)
-    mmpicklist.to_csv(date+'mmpicklist.csv',index=False)
+
+    ppdest = date + 'plasmidpicklist'
+    counter = 1
+    while os.path.isfile(ppdest+'.csv'):
+        ppdest = ppdest + '_' + str(counter)
+        counter += 1
+
+    mmdest = date + 'mmpicklist'
+    counter = 1
+    while os.path.isfile(mmdest+'.csv'):
+        mmdest = mmdest + '_' + str(counter)
+        counter += 1
+
+    plasmidpicklist.to_csv(ppdest+'.csv',index=False)
+    mmpicklist.to_csv(mmdest+'.csv',index=False)
     #Save the source plates to a file
-    pkl.dump(allsourcepl, open(date + dname, "wb"),-1)
+    dumpdest = date + dname
+    counter = 1
+    while os.path.isfile(dumpdest+'.p'):
+        dumpdest = dumpdest + '_' + str(counter)
+        counter += 1
+
+    pkl.dump(allsourcepl, open(dumpdest +'.p', "wb"),-1)
 
     return destinationloc, plasmidpicklist, mmpicklist
 
@@ -172,17 +201,14 @@ if __name__ == "__main__":
         help = 'File name of source plate')
     parser.add_argument('-p', action='store', dest = 'platetype', type=str, default = None,
         help = 'Plate Type')
-    parser.add_argument('-d', action='store', dest = 'dname', type=str, default = 'sourceplates.p',
-        help = 'Destination for new source plate file')
+    parser.add_argument('-d', action='store', dest = 'dname', type=str, default = 'sourceplates',
+        help = 'Destination for new source plate file (no need for .p ext)')
 
     args = vars(parser.parse_args())
     rawdf = import_instructions(args['fname'])
     counts = getparts(rawdf)
     allsourcepl, userinstr = checksourceplate(counts,args['sname'],args['platetype'])
     destinationloc, plasmidpicklist, mmpicklist = makepicklists(rawdf,allsourcepl,args['dname'])
-
-    print userinstr
-    print destinationloc
 
     print "{:<8} {:<15}".format('Plasmid','Instructions')
     for k, v in userinstr.iteritems():
